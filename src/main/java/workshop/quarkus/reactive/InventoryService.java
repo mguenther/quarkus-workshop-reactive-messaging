dd5892db5af3fb076c8a9c8e663ef01c13b9c7fe
Markus Günther <markus.guenther@gmail.com>
2023-06-12 14:43:47 +0200

    Adds solution to task 2

--- src/main/java/workshop/quarkus/reactive/InventoryService.java
+++ src/main/java/workshop/quarkus/reactive/InventoryService.java
@@ -2,17 +2,50 @@ package workshop.quarkus.reactive;
 
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import workshop.quarkus.reactive.event.Event;
 import workshop.quarkus.reactive.event.OrderApprovedEvent;
+import workshop.quarkus.reactive.event.OrderDeniedEvent;
 import workshop.quarkus.reactive.event.OrderSubmittedEvent;
+
+import java.time.Clock;
+import java.time.Instant;
 
 @ApplicationScoped
 public class InventoryService {
 
+    @Inject
+    InventoryAvailabilityChecker checker;
+
     @Incoming("incoming-orders")
     @Outgoing("outgoing-checked-orders")
-    public Uni<OrderApprovedEvent> checkAvailability(OrderSubmittedEvent event) {
-        return Uni.createFrom().nullItem();
+    public Event checkAvailability(Message<OrderSubmittedEvent> message) {
+
+        var e = message.getPayload();
+        var traceId = message.getMetadata(OrderMetadata.class).map(OrderMetadata::traceId).orElse("no-trace-id");
+        var available = checker.allLineItemsAvailable(traceId);
+
+        try {
+            return available ?
+                    new OrderApprovedEvent(
+                            e.getOrderId(),
+                            e.getCustomerId(),
+                            e.getProductId(),
+                            e.getQuantity(),
+                            true,
+                            Instant.now(Clock.systemUTC()).toEpochMilli()) :
+                    new OrderDeniedEvent(
+                            e.getOrderId(),
+                            e.getCustomerId(),
+                            e.getProductId(),
+                            e.getQuantity(),
+                            false,
+                            Instant.now(Clock.systemUTC()).toEpochMilli());
+        } finally {
+            message.ack();
+        }
     }
 }
