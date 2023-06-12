0be5226c00bc8f7e136558a97c37b4da95e2dd70
Markus Günther <markus.guenther@gmail.com>
2023-06-12 14:42:48 +0200

    Adds solution to task 1

--- src/main/java/workshop/quarkus/reactive/OrderService.java
+++ src/main/java/workshop/quarkus/reactive/OrderService.java
@@ -1,13 +1,26 @@
 package workshop.quarkus.reactive;
 
 import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Metadata;
 import workshop.quarkus.reactive.command.PlaceOrder;
+import workshop.quarkus.reactive.event.OrderSubmittedEvent;
 
 @ApplicationScoped
 public class OrderService {
 
+    @Channel("outgoing-orders")
+    MutinyEmitter<OrderSubmittedEvent> orderChannel;
+
     public Uni<Void> submitOrder(PlaceOrder order) {
-        return Uni.createFrom().voidItem();
+
+        return Uni.createFrom().item(order)
+                .map(o -> new OrderSubmittedEvent(o.orderId(), o.customerId(), o.productId(), o.quantity()))
+                .map(Message::of)
+                .map(m -> m.withMetadata(Metadata.of(new OrderMetadata(order.traceId()))))
+                .flatMap(orderChannel::sendMessage);
     }
 }
